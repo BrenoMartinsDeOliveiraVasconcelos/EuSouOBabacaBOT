a0481893b990e146bc1e912a1f2f71e1a180cd60
BrenoMartinsDeOliveiraVasconcelos <breno05martins@gmail.com>
2022-06-30 12:26:15 -0300

    log

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import praw
 import time
 import datetime
 import json
+import tools
 
 
 login = json.load(open("login"))
@@ -32,89 +33,87 @@ OS VOTOS SÃO CONTADOS COM ALGUNS MINUTOS DE ATRASO, ENTÃO TENHAM PACIÊNCIA!
 ^(Eu sou um robô e esse comentário foi feito automáticamente. Beep bop!) 
 ^([Códiigdo fonte](https://github.com/BrenoMartinsDeOliveiraVasconcelos/EuSouOBabacaBOT))
 """
-while True:
-    currentime = datetime.datetime.now().strftime("%H:%M")
-    subcount = 0
-    try:
-        submissons = reddit.subreddit('EuSOuOBabaca').new(limit=100)
-        time.sleep(1)
-        for submission in submissons:
-            subcount += 1
-            print(f"== {subcount} ==")
-            assholecount = {
-                            "NEOB": 0,
-                            "EOB": 0,
-                            "NGM": 0,
-                            "TEOB": 0,
-                            "INFO": 0
-                            }
-            sublist = open('idlist', 'r').readlines()
-            indx = -1
-            for i in sublist:
-                indx += 1
-                sublist[indx] = i.strip()
-            if submission.id not in sublist:
-                botcomment = submission.reply(body=botxt)
-                botcomment.mod.distinguish(sticky=True)
-                print("a")
-                sublist.append(submission.id)
-                with open('idlist', 'a') as f:
-                    f.write(submission.id + '\n')
-                print('Comentário enviado para ' + submission.id)
-            comments = submission.comments.list()
-            if currentime: #== "15:07":
-                highest = 0
-                key = ''
-                for comment in comments:
-                    if comment.author != 'EuSouOBabacaBOT':
-                        comment_body = comment.body.split(' ')
-                        indx = -1
-                        for i in comment_body:
-                            indx += 1
-                            i = i.split("\n")
-                            comment_body[indx] = i[0]
-                            try:
-                                comment_body.insert(indx+1, i[1])
-                            except IndexError:
-                                pass
-                        rate = []
-                        for i in comment_body:
-                            i = i.strip()
-                            replaces = ["!", "?", ".", ",", ":", "(", ")", "[", "]", "{", "}", "-",
-                                        "+", "/", "\\", "'", '"', '~']
-                            for c in replaces:
-                                i = i.replace(c, "")
-                            print(i)
-                            rate.append(i)
-                        #= ''.join(rate).upper()
-                        print(rate)
-                        rates = ["NEOB", "EOB", "TEOB", "NGM", "INFO"]
-                        indx =-1
-                        for w in rate:
-                            indx += 1
-                            rate[indx] = w.upper().strip()
-                        for r in rates:
-                            if r in rate:
-                                assholecount[r] += 1
-                                print("Ok")
-                        #if rate in ["NEOB", "EOB", "TEOB", "NGM", "INFO"]:
-                        #    print("OK")
-                        #    assholecount[rate] += 1
-                            # print(assholecount)
-                                counted = 1
-                                break
+
+
+def runtime():
+    while True:
+        currentime = datetime.datetime.now().strftime("%H:%M")
+        subcount = 0
+        try:
+            submissons = reddit.subreddit('EuSOuOBabacaBOT').new(limit=100)
+            time.sleep(1)
+            for submission in submissons:
+                subcount += 1
+                print(f"== {subcount} ==")
+                assholecount = {
+                                "NEOB": 0,
+                                "EOB": 0,
+                                "NGM": 0,
+                                "TEOB": 0,
+                                "INFO": 0
+                                }
+                sublist = open('idlist', 'r').readlines()
+                indx = -1
+                for i in sublist:
+                    indx += 1
+                    sublist[indx] = i.strip()
+                if submission.id not in sublist:
+                    botcomment = submission.reply(body=botxt)
+                    tools.logger(0, sub_id=submission.id)
+                    botcomment.mod.distinguish(sticky=True)
+                    sublist.append(submission.id)
+                    with open('idlist', 'a') as f:
+                        f.write(submission.id + '\n')
+                comments = submission.comments.list()
+                if currentime:
+                    highest = 0
+                    key = ''
+                    for comment in comments:
+                        if comment.author != 'EuSouOBabacaBOT':
+                            comment_body = comment.body.split(' ')
+                            indx = -1
+                            for i in comment_body:
+                                indx += 1
+                                i = i.split("\n")
+                                comment_body[indx] = i[0]
+                                try:
+                                    comment_body.insert(indx+1, i[1])
+                                except IndexError:
+                                    pass
+                            rate = []
+                            for i in comment_body:
+                                i = i.strip()
+                                replaces = ["!", "?", ".", ",", ":", "(", ")", "[", "]", "{", "}", "-",
+                                            "+", "/", "\\", "'", '"', '~']
+                                for c in replaces:
+                                    i = i.replace(c, "")
+                                rate.append(i)
+                            rates = ["NEOB", "EOB", "TEOB", "NGM", "INFO"]
+                            indx = -1
+                            for w in rate:
+                                indx += 1
+                                rate[indx] = w.upper().strip()
+                            for r in rates:
+                                if r in rate:
+                                    assholecount[r] += 1
+                                    counted = 1
+                                    break
+                            else:
+                                counted = 0
+                            for k, v in assholecount.items():
+                                if v >= highest:
+                                    highest = v
+                                    key = k
+                            if counted == 1:
+                                for com in submission.comments.list():
+                                    if com.author == "EuSouOBabacaBOT":
+                                        com.edit(body=f"# VEREDITO ATUAL: {key} ({highest} votos)\n"+botxt)
+                                        tools.logger(1, sub_id=submission.id)
                         else:
-                            counted = 0
-                        for k, v in assholecount.items():
-                            if v >= highest:
-                                highest = v
-                                key = k
-                        if counted == 1:
-                            for com in submission.comments.list():
-                                if com.author == "EuSouOBabacaBOT":
-                                    com.edit(body=f"# VEREDITO ATUAL: {key} ({highest} votos)\n"+botxt)
-                                    print("Comentário editado com sucesso")
-                    else:
-                        pass
-    except Exception as e:
-        print(f"{e}")
+                            pass
+        except Exception as e:
+            tools.logger(2, ex=e)
+
+
+if __name__ == '__main__':
+    runtime()
---
+++ tools.py
@@ -0,0 +1,11 @@
+import datetime
+
+
+def logger(tp, sub_id="", ex=""):
+    current_time = datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
+    if tp == 0:
+        print(f"[{current_time}] Comentário enviado: {sub_id}")
+    elif tp == 1:
+        print(f"[{current_time}] Comentário editado em {sub_id}")
+    elif tp == 2:
+        print(f"[{current_time}] {ex}")
