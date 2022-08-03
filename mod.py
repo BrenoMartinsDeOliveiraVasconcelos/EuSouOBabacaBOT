f9f8429ad229f0ddb40cf49dbc7cac5fbcf542a6
Breno <breno05martins@gmail.com>
2022-08-03 15:01:57 -0300

    censor updayete

--- mod.py
+++ mod.py
@@ -16,14 +16,12 @@ reddit = praw.Reddit(
     password=login["password"]
 )
 
-
-print("INIT MOD!!!")
 while True:
     currentime = datetime.datetime.now().strftime("%H:%M")
-    try:
-        subcount = 0
-        submissons = reddit.subreddit('EuSOuOBabaca').new(limit=100)
-        for submission in submissons:
+    subcount = 0
+    submissons = reddit.subreddit('EuSOuOBabaca').new(limit=100)
+    for submission in submissons:
+        try:
             time.sleep(1)
             subcount += 1
             print("MOD UPDATE")
@@ -32,7 +30,7 @@ while True:
             indx = -1
             for i in sublist:
                 indx += 1
-                sublist[indx] = i.strip()   
+                sublist[indx] = i.strip().replace("\n", "")  
             if submission.id not in sublist:
                 subauthor = submission.author
                 if subauthor.comment_karma + subauthor.link_karma < 0:
@@ -41,13 +39,26 @@ while True:
                     tools.logger(tp=4, sub_id=submission.id, reason="Karma negativo")
                 subtxt = submission.selftext
                 subtxt = subtxt.split(" ")
+                badwords = open("palavroes", "r").readlines()
+                bd = ""
+                indx = -1
+                for i in badwords:
+                    indx += 1
+                    i = i.strip().replace("\n", "").lower()
+                    badwords[indx] = i
                 for i in subtxt:
                     i = i.strip().replace(" ", "").replace("\n", "")
-                    print(i)
                     if i.startswith("[http") and r"//" in i:
                         submission.mod.remove(spam=True)
                         submission.reply(body="Não é permitido links nesse subreddit! Sua publicação foi removida.")
                         tools.logger(tp=4, sub_id=submission.id, reason="Link")
+                        break
+                    if i.lower() in badwords:
+                        bd = i.lower()
+                        submission.mod.remove(spam=False)
+                        submission.reply(body=f"Por favor mantenha o respeito ao contar sua história! Post removido pois contêm palavrão ({bd}).")
+                        tools.logger(tp=4, sub_id=submission.id, reason=f"Falta de respeito. ({bd})")
+                        break
                 open("modlist", "a").write(submission.id + "\n")
-    except Exception as e:
-        tools.logger(2, e)
+        except Exception as e:
+            tools.logger(2, ex=e)
