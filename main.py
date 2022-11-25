ba47c2e8ab22cd6ad44fdab99c4583b506180314
Breno Martins <crdj161@gmail.com>
2022-11-25 16:34:21 -0300

    /ping

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ import multiprocessing
 import discord
 from discord.ext import commands
 import datetime
+import os
+import psutil
+
 # import os
 
 # multiprocessing.Process(target=os.system, args=["python3 mod.py",]).start()
@@ -50,136 +53,139 @@ def runtime():
         was_in_sub = True
         currentime = datetime.datetime.now().strftime("%H:%M")
         subcount = 0
-        try:
-            submissons = reddit.subreddit('EuSOuOBabaca').new(limit=login["submissions"])
-            for submission in submissons:
-                rst = open("restart", "r").readlines()
-                if rst[0] == "1":
-                    open("restart", "w+").write("0")
-                    tools.logger(tp=2, ex="Bot reiniciado remotamente.")
-                    break
-                subcount += 1
-                tools.logger(tp=3, num=subcount)
-                assholecount = {
-                                "NEOB": 0,
-                                "EOB": 0,
-                                "NGM": 0,
-                                "TEOB": 0,
-                                "INFO": 0,
-                                "FANFIC": 0
-                                }
-                sublist = open('idlist', 'r').readlines()
-                indx = -1
-                for i in sublist:
-                    indx += 1
-                    sublist[indx] = i.strip()
-                if submission.id not in sublist:
-                    reddit.validate_on_submit = True
-                    botcomment = submission.reply(body=botxt)
-                    # redditor = submission.author
-                    tools.logger(0, sub_id=submission.id)
-                    botcomment.mod.distinguish(sticky=True)
-                    botcomment.mod.lock()
-                    botcomment.mod.approve()
-                    sublist.append(submission.id)
-                    was_in_sub = False
-                    with open('idlist', 'a') as f:
-                        f.write(submission.id + '\n')
-                submission.comments.replace_more(limit=None)
-                comments = submission.comments.list()
-                if currentime:
-                    highest = 0
-                    key = ''
-                    users = []
-                    fanficout = 0
-                    for comment in comments:
-                        try:
-                            if comment.author != 'EuSouOBabacaBOT' and comment.author not in users:
-                                users.append(comment.author)
-                                comment_body = comment.body.split(' ')
-                                indx = -1
-                                for i in comment_body:
-                                    indx += 1
-                                    i = i.split("\n")
-                                    comment_body[indx] = i[0]
-                                    try:
-                                        comment_body.insert(indx+1, i[1])
-                                    except IndexError:
-                                        pass
-                                rate = []
-                                for i in comment_body:
-                                    i = i.strip()
-                                    replaces = ["!", "?", ".", ",", ":", "(", ")", "[", "]", "{", "}", "-",
-                                                "+", "/", "\\", "'", '"', '~']
-                                    for c in replaces:
-                                        i = i.replace(c, "")
-                                    rate.append(i)
-                                rates = ["NEOB", "EOB", "TEOB", "NGM", "INFO", "FANFIC"]
-                                indx = -1
-                                for w in rate:
-                                    indx += 1
-                                    rate[indx] = w.upper().strip()
-                                for r in rates:
-                                    if r in rate:
-                                        assholecount[r] += 1
-                                        counted = 1
-                                        break
-                                else:
-                                    counted = 0
-                                total = 0
-                                for k, v in assholecount.items():
-                                    total += v
-                                    if v >= highest:
-                                        highest = v
-                                        key = k
+        submissons = reddit.subreddit('EuSOuOBabaca').new(limit=login["submissions"])
+        for submission in submissons:
+            rst = open("restart", "r").readlines()
+            if rst[0] == "1":
+                open("restart", "w+").write("0")
+                tools.logger(tp=2, ex="Bot reiniciado remotamente.")
+                break
+            subcount += 1
+            open("last", "w+").write(f"{subcount}")
+            tools.logger(tp=3, num=subcount)
+            assholecount = {
+                "NEOB": 0,
+                "EOB": 0,
+                "NGM": 0,
+                "TEOB": 0,
+                "INFO": 0,
+                "FANFIC": 0
+            }
+            sublist = open('idlist', 'r').readlines()
+            indx = -1
+            for i in sublist:
+                indx += 1
+                sublist[indx] = i.strip()
+            if submission.id not in sublist:
+                reddit.validate_on_submit = True
+                botcomment = submission.reply(body=botxt)
+                # redditor = submission.author
+                tools.logger(0, sub_id=submission.id)
+                botcomment.mod.distinguish(sticky=True)
+                botcomment.mod.lock()
+                botcomment.mod.approve()
+                sublist.append(submission.id)
+                was_in_sub = False
+                with open('idlist', 'a') as f:
+                    f.write(submission.id + '\n')
+            submission.comments.replace_more(limit=None)
+            comments = submission.comments.list()
+            if currentime:
+                highest = 0
+                key = ''
+                users = []
+                fanficout = 0
+                for comment in comments:
+                    try:
+                        if comment.author != 'EuSouOBabacaBOT' and comment.author not in users:
+                            users.append(comment.author)
+                            comment_body = comment.body.split(' ')
+                            indx = -1
+                            for i in comment_body:
+                                indx += 1
+                                i = i.split("\n")
+                                comment_body[indx] = i[0]
                                 try:
-                                    percent = highest/total
-                                except ZeroDivisionError:
-                                    percent = 1.00
-                                if counted == 1:
-                                    for com in submission.comments.list():
-                                        if com.author == "EuSouOBabacaBOT":
-                                            judgment = "Não é o babaca" if key == "NEOB" else \
-                                                "É o babaca" if key == "EOB" else \
-                                                "Todo mundo é babaca" if key == "TEOB" else \
-                                                "Ninguém é o babaca" if key == "NGM" else \
-                                                "Falta informação" if key == "INFO" else \
-                                                "Fanfic"
-
-                                            match key:
-                                                case 'NEOB':
-                                                    submission.flair.select("fad0940c-6841-11ed-baed-365116e43406")
-                                                case 'EOB':
-                                                    submission.flair.select("e46b8208-6841-11ed-99cd-cec761e4d61c")
-                                                case 'TEOB':
-                                                    submission.flair.select("8cb95bb0-6842-11ed-9cdf-a2c7df914eb2")
-                                                case 'NGM':
-                                                    submission.flair.select("3704e1da-6842-11ed-924e-e273ede5d967")
-                                                case 'INFO':
-                                                    submission.flair.select("562808bc-6842-11ed-8dd7-86bf8dba8041")
-                                                case 'FANFIC':
-                                                    fanficout += 1
-                                                    if not was_in_sub:
-                                                        if fanficout >= 4:
-                                                            submission.flair.select("eb374206-6842-11ed-96dc"
-                                                                                    "-d2448cda5278")
-                                                            submission.report("Suspeita de fanfic!")
-                                                        elif fanficout >= 8:
-                                                            submission.mod.remove(spam=False)
-
-                                            com.edit(body=f"# Veredito atual: {judgment} ({percent*100:.2f}% de {total}"
-                                                          f" "
-                                                          f""
-                                                          f"votos)\nÚltima atualização feita em: "
-                                                          f"{datetime.datetime.now().strftime('%d/%m/%Y às %H:%M')}"
-                                                          f"\n\n"+botxt)
-                                            tools.logger(1, sub_id=submission.id)
+                                    comment_body.insert(indx + 1, i[1])
+                                except IndexError:
+                                    pass
+                            rate = []
+                            for i in comment_body:
+                                i = i.strip()
+                                replaces = ["!", "?", ".", ",", ":", "(", ")", "[", "]", "{", "}", "-",
+                                            "+", "/", "\\", "'", '"', '~']
+                                for c in replaces:
+                                    i = i.replace(c, "")
+                                rate.append(i)
+                            rates = ["NEOB", "EOB", "TEOB", "NGM", "INFO", "FANFIC"]
+                            indx = -1
+                            for w in rate:
+                                indx += 1
+                                rate[indx] = w.upper().strip()
+                            for r in rates:
+                                if r in rate:
+                                    assholecount[r] += 1
+                                    counted = 1
+                                    break
                             else:
-                                pass
-                        except Exception as e:
-                            tools.logger(2, ex=e)
-        except Exception as e:
-            tools.logger(2, ex=e)
+                                counted = 0
+                            total = 0
+                            for k, v in assholecount.items():
+                                total += v
+                                if v >= highest:
+                                    highest = v
+                                    key = k
+                            try:
+                                percent = highest / total
+                            except ZeroDivisionError:
+                                percent = 1.00
+                            if counted == 1:
+                                for com in submission.comments.list():
+                                    if com.author == "EuSouOBabacaBOT":
+                                        judgment = "Não é o babaca" if key == "NEOB" else \
+                                            "É o babaca" if key == "EOB" else \
+                                            "Todo mundo é babaca" if key == "TEOB" else \
+                                            "Ninguém é o babaca" if key == "NGM" else \
+                                            "Falta informação" if key == "INFO" else \
+                                            "Fake"
+
+                                        match key:
+                                            case 'NEOB':
+                                                submission.flair.select("fad0940c-6841-11ed-baed-365116e43406")
+                                            case 'EOB':
+                                                submission.flair.select("e46b8208-6841-11ed-99cd-cec761e4d61c")
+                                            case 'TEOB':
+                                                submission.flair.select("8cb95bb0-6842-11ed-9cdf-a2c7df914eb2")
+                                            case 'NGM':
+                                                submission.flair.select("3704e1da-6842-11ed-924e-e273ede5d967")
+                                            case 'INFO':
+                                                submission.flair.select("562808bc-6842-11ed-8dd7-86bf8dba8041")
+                                            case 'FANFIC':
+                                                fanficout += 1
+                                                if not was_in_sub:
+                                                    if fanficout >= 4:
+                                                        submission.flair.select("eb374206-6842-11ed-96dc"
+                                                                                "-d2448cda5278")
+                                                        submission.report("Suspeita de fanfic!")
+                                                    elif fanficout >= 8:
+                                                        submission.mod.remove(spam=False)
+
+                                        if percent < 0.50:
+                                            judgment = "Inconclusivo"
+                                            votetxt = f"{total} votos contados ao total"
+                                            submission.flair.select("17ace5be-6cd2-11ed-880b-f6403a5de3db")
+                                        else:
+                                            votetxt = f"{percent * 100:.2f}% de {total} votos"
+                                        ftxt = f"# Veredito atual:" \
+                                            f" {judgment} ({votetxt})\n\nÚltima atualização feita em: " \
+                                               f"{datetime.datetime.now().strftime('%d/%m/%Y às %H:%M')}\n\n "
+                                        com.edit(
+                                            body=ftxt + botxt)
+                                        tools.logger(1, sub_id=submission.id)
+                        else:
+                            pass
+                    except Exception as e:
+                        tools.logger(2, ex=e)
 
 
 if __name__ == '__main__':
@@ -191,9 +197,23 @@ if __name__ == '__main__':
     discord_token = login["discord_token"]
 
 
-    @bot.command(name="ping", help="Ping!")
+    @bot.command(name="ping", help="Pong!")
     async def ping(ctx):
         await ctx.send("Pong!")
+        pingdict = {
+            "is_alive": bot_thread.is_alive(),
+            "current_post": open("last", "r").readline().strip("\n"),
+            "log_size": f'{os.stat("log").st_size / 1024:.0f} kb',
+            "reddit_thread_pid": bot_thread.pid,
+            "discord_pid": os.getpid(),
+            "memory_d": f"{psutil.Process(os.getpid()).memory_info().rss / 1024**2:.0f} mb",
+        }
+
+        pingdict["memory_rd"] = f"{psutil.Process(pingdict['reddit_thread_pid']).memory_info().rss / 1024**2:.0f} mb"
+        pingdict["memory_total"] = f"{(psutil.Process(os.getpid()).memory_info().rss / 1024**2) + (psutil.Process(pingdict['reddit_thread_pid']).memory_info().rss / 1024**2):.0f} mb "
+
+        for k, v in pingdict.items():
+            await ctx.send(f"{k} = {v}")
 
 
     @bot.command(name="reiniciar", help="Reinicia a sessão no reddit")
@@ -204,7 +224,7 @@ if __name__ == '__main__':
 
 
     @bot.command(name="log", help="Retorna o arquivo de log caso não tenha um argumento"
-                                    "\nCaso tenha o argumento 'limpar', limpa o arquivo de log")
+                                  "\nCaso tenha o argumento 'limpar', limpa o arquivo de log")
     async def send_log(ctx, *args):
         if len(args) == 0:
             await ctx.send("Aqui está!")
@@ -215,9 +235,9 @@ if __name__ == '__main__':
 
 
     @bot.command(name="fechar", help="Fecha o programa ou o bot.\n"
-                                        "\n'bot' fecha apenas a sessão do bot"
-                                        "\np'programa' fecha o programa inteiro. Pode ser"
-                                        "necessário entrar no servidor para iniciar de novo.")
+                                     "\n'bot' fecha apenas a sessão do bot"
+                                     "\np'programa' fecha o programa inteiro. Pode ser"
+                                     "necessário entrar no servidor para iniciar de novo.")
     async def close(ctx, *args):
         if len(args) > 0:
             tools.logger(tp=2, ex="Bot ou programa fechado remotamente.")
@@ -233,7 +253,7 @@ if __name__ == '__main__':
 
 
     @bot.command(name="configurar", help="Troca a configuração no arquivo json 'config'. 'mostrar' para "
-                                            "mostrar as configurações atuais. <key> <value> para alterar.")
+                                         "mostrar as configurações atuais. <key> <value> para alterar.")
     async def config(ctx, *args):
         if len(args) >= 2:
             login[args[0]] = args[1]
