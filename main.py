1d269953c35232acf74621528bec548b192120c5
breno <breno05martins@gmail.com>
2023-06-03 10:11:07 -0300

    Lucasbot v3

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ import shutil
 import time
 import datetime
 import traceback
+import random
+import psutil
+import os
 
 config = json.load(open('config.json', 'r'))
 api = json.load(open("api.json"))
+splashes = json.load(open('splashes.json', 'r'))
 
 settings = {
     "clientid": api["clientid"],
@@ -18,8 +22,11 @@ settings = {
     "useragent": api["useragent"],
     "submissions": config['submissions'],
     "subreddit": config["subreddit"],
-    "backup": config["backup"]
+    "backup": config["backup"],
+    "clear_log": config["clear_log"]
 }
+
+flairs = config["flairs"]
 
 reddit = praw.Reddit(
     user_agent=settings["useragent"],
@@ -30,62 +37,46 @@ reddit = praw.Reddit(
 )
 
 # Post a comment to every new reddit submission in r/EuSOuOBabaca
-botxt = \
-    """Vou contar as respostas que as pessoas dão nesse post! Pra ser contado, responda com essas siglas o post:
+botxt = f"\n\n**{config['upper_text']}**\n\nVou contar as respostas que as pessoas dão nesse post! Pra ser contado, responda com essas siglas o post:\n\n"
 
-NEOB - Não é o babaca (E o resto sim)
+votxt = ["", "", ""]
+for k, v in flairs.items():
+    votxt[v[1]] += f"{k} - {v[2]}\n\n"
 
-EOB - É o babaca 
+botxt += votxt[0] + "**Votos especiais**\n\n" + votxt[1] + "\n\n"
 
-NGM - Ninguem (é o babaca) 
+botxt+= "##Nota: Pode demorar cerca de 2 horas para atualizar!\n\n"
 
-TEOB - Todo mundo é o babaca 
-
-INFO - Falta informaçã
-
-FANFIC - Quando algo é obviamente falso 
-
-Demora um pouco até eu contar tudo, então espere alguns minutos... Ou sei lá, tanto faz.
-
-Nota: Eu não conto respostas a comentários, somente comentários.
-"""
-
+joke = ""
 
 def runtime():
     reddit.validate_on_submit = True
-    current_loop = int("".join(open("last", "r").readlines()).replace('\n', ''))
     while True:
-        current_loop += 1
         try:
             ftxt = f"# Veredito atual:" \
                    f" Não processado ainda \n\nÚltima atualização feita em: " \
                    f"{datetime.datetime.now().strftime('%d/%m/%Y às %H:%M')}\n\n "
-            etxt = """
-            
-^(Eu sou um robô e esse comentário foi feito automáticamente. Beep bop!) 
-^(Lucas Bot v2.0 - by [JakeWisconsin](https://www.reddit.com/u/JakeWisconsin))"""
             subcount = 0
             submissons = reddit.subreddit(settings["subreddit"]).new(limit=int(settings["submissions"]))
             adds = ""
             edits = ""
             flairchanges = ""
             table = ""
-            atime = datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
+            atime = datetime.datetime.now().timestamp()
 
             for submission in submissons:
-                assholecount = {
-                    "NEOB": 0,
-                    "EOB": 0,
-                    "NGM": 0,
-                    "TEOB": 0,
-                    "INFO": 0,
-                    "FANFIC": 0,
-                }
-                rst = open("restart", "r").readlines()
-                if rst[0] == "1":
-                    open("restart", "w+").write("0")
-                    tools.logger(tp=2, ex="Bot reiniciado remotamente.")
-                    break
+                
+                joke = random.choice(splashes)
+                etxt = f"""
+                                
+*{joke}* 
+*{config['info']['name']} v{config['info']['version']} - by [{config['info']['creator']}](https://www.reddit.com/u/{config['info']['creator']})*"""
+                
+                assholecount = {}
+                for k in flairs.keys():
+                    if k not in config["flairs_ignore"]:
+                        assholecount[k] = 0
+
                 subcount += 1
                 tools.logger(tp=3, num=subcount, sub_id=submission.id)
                 sublist = open('idlist', 'r').readlines()
@@ -99,7 +90,7 @@ def runtime():
                     botcomment.mod.distinguish(sticky=True)
                     botcomment.mod.approve()
                     sublist.append(submission.id)
-                    submission.flair.select("5462ec04-70c6-11ed-8642-aa07fd483ac4")
+                    submission.flair.select(flairs["NOT_CLASSIFIED"][0])
                     with open('idlist', 'a') as f:
                         f.write(submission.id + '\n')
                     adds += f"\n* Adicionado https://www.reddit.com/{submission.id} a lista de ids.\n"
@@ -111,12 +102,14 @@ def runtime():
                 users = []
                 total = 0
                 judgment = ""
-
+                percent = 0
+                rates = [x for x in assholecount.keys()]
+                judges = config["vote_name"]
                 for comment in comments:
                     try:
                         if comment.author != settings["username"] and comment.author not in users \
                                 and comment.author != submission.author:
-                            comment_body = comment.body.split(' ')
+                            comment_body = comment.body.split(' ')   
                             indx = -1
                             for i in comment_body:
                                 indx += 1
@@ -134,7 +127,7 @@ def runtime():
                                 for c in replaces:
                                     i = i.replace(c, "")
                                 rate.append(i)
-                            rates = ["NEOB", "EOB", "TEOB", "NGM", "INFO", "FANFIC"]
+                            
                             indx = -1
                             for w in rate:
                                 indx += 1
@@ -152,12 +145,16 @@ def runtime():
                             try:
                                 percent = highest / total
                             except ZeroDivisionError:
-                                percent = 1.00
-                            judgment = "Não é o babaca" if key == "NEOB" else \
+                                percent = 1.00 
+
+                            '''judgment = "Não é o babaca" if key == "NEOB" else \
                                 "É o babaca" if key == "EOB" else \
                                 "Todo mundo é babaca" if key == "TEOB" else \
                                 "Ninguém é o babaca" if key == "NGM" else \
-                                "Falta informação" if key == "INFO" else "Fake"
+                                "Falta informação" if key == "INFO"  "FANFIC" "Fake"'''
+                            
+                            ind = rates.index(key)
+                            judgment = judges[ind]
 
                             if percent < 0.50:
                                 judgment = "Inconclusivo"
@@ -172,8 +169,8 @@ def runtime():
                                    f"{judgment} ({votetxt})\n\nÚltima atualização feita em: " \
                                    f"{datetime.datetime.now().strftime('%d/%m/%Y às %H:%M')}\n\n "
                             users.append(comment.author)
-                    except Exception as e:
-                        tools.logger(2, ex=e)
+                    except Exception:
+                        tools.logger(5, ex=traceback.format_exc())
 
                 percents = {}
                 for k, v in assholecount.items():
@@ -181,42 +178,39 @@ def runtime():
                         percents[k] = f"{(int(v) / total) * 100:.2f}"
                     except ZeroDivisionError:
                         percents[k] = f"0.00"
-                with open(f"rates/{submission.id}.json", "w") as json_file:
-                    json.dump(assholecount, json_file, indent=4)
                 tools.logger(2, ex="Submissão analizada!")
 
-                etxt = f"""
+                votxt = f"""
 # Tabela de votos
 Voto | Quantidade | %
 :--:|:--:|:--:
 """
+
                 for k, v in assholecount.items():
-                    etxt += f"{k} | {v} | {percents[k]}%\n"
-                table += f"\n* A tabela de https://www.reddit.com/{submission.id} agora é:\n\n{etxt}\n"
-                etxt += """
-                                
-^(Eu sou um robô e esse comentário foi feito automáticamente. Beep bop!) 
-^(Lucas Bot v2.0 - by [JakeWisconsin](https://www.reddit.com/u/JakeWisconsin))"""
-                match judgment:
-                    case 'Não é o babaca':
-                        submission.flair.select("fad0940c-6841-11ed-baed-365116e43406")
-                    case 'É o babaca':
-                        submission.flair.select("e46b8208-6841-11ed-99cd-cec761e4d61c")
-                    case 'Todo mundo é babaca':
-                        submission.flair.select("8cb95bb0-6842-11ed-9cdf-a2c7df914eb2")
-                    case 'Ninguém é o babaca':
-                        submission.flair.select("3704e1da-6842-11ed-924e-e273ede5d967")
-                    case 'Falta informação':
-                        submission.flair.select("562808bc-6842-11ed-8dd7-86bf8dba8041")
-                    case "Inconclusivo":
-                        submission.flair.select("17ace5be-6cd2-11ed-880b-f6403a5de3db")
-                    case "Não avaliado":
-                        submission.flair.select("528e0f44-7017-11ed-bf35-7a08e652fb3d")
-                    case "Fake":
-                        submission.flair.select("5c55d140-700a-11ed-8d83-1e3195d8e0d4")
+                    votxt += f"{k} | {v} | {percents[k]}%\n"
+                
+                etxt = votxt + etxt
+                if percent >= 0.5 and total > 0:
+                    submission.flair.select(flairs[key][0])
+                    if key in ["FANFIC", "OT"]:
+                        removes = open('rid', "r").readlines()
 
-                        
+                        indx = -1
+                        for i in removes:
+                            indx += 1
+                            removes[indx] = i.strip()
 
+                        if submission.id not in removes and total > 1:
+                            submission.mod.remove(mod_note="Contra as regras", spam=False)
+                            submission.reply(body=f"Seu post recebeu {percent*100}% de votos '{key}' portanto foi"
+                                                f" removido por suspeita de violar as regras do subreddit. Se houve um "
+                                                "engano, por favor não hesite em mandar um modmail para a moderação.")
+                            tools.logger(tp=4, sub_id=submission.id, reason="VIolação")
+                            open("rid", "a").write(f"{submission.id}\n")
+                elif percent < 0.5 and total > 0:
+                    submission.flair.select(flairs["INCONCLUSIVE"][0])
+                elif total == 0:
+                    submission.flair.select(flairs["NOT_AVALIABLE"][0])
                 flairchanges += f"\n* Flair de https://www.reddit.com/{submission.id} é '{judgment}'"
                 tools.logger(2, ex=f"Flair editada em {submission.id}")
                 for com in comments:
@@ -232,33 +226,10 @@ Voto | Quantidade | %
                 ftxt = f"# Veredito atual:" \
                        f" Não disponível \n\nÚltima atualização feita em: " \
                        f"{datetime.datetime.now().strftime('%d/%m/%Y às %H:%M')}\n\n "
-                btime = datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
-            satxt = f""""# Adioções na lista de ids:
-{adds}"""
-            setxt = f"""
-# Edições de comentário:
-{edits}"""
-            sftxt = f""" 
-# Edições de flair de posts:
-{flairchanges}
-"""
-            sttxt = f""" 
-# Tabelas
-{table}
-"""
-            reddit.subreddit("EuSouOBabacaBOT").submit(f"[ADD] LOG PARA O LOOP NUMERO {current_loop} ({atime} até {btime})",
-                                                       selftext=satxt)
-            reddit.subreddit("EuSouOBabacaBOT").submit(
-                f"[EDIT] LOG PARA O LOOP NUMERO {current_loop} ({atime} até {btime})",
-                selftext=setxt)
-            reddit.subreddit("EuSouOBabacaBOT").submit(
-                f"[FLAIR] LOG PARA O LOOP NUMERO {current_loop} ({atime} até {btime})",
-                selftext=sftxt)
-            open("last", "w+").write(f"{current_loop}")
+            btime = datetime.datetime.now().timestamp()
+            tools.logger(2, ex=f"runtime(): {btime-atime}s", bprint=False)
         except Exception as e:
-            tools.logger(2, ex=traceback.format_exc())
-            reddit.subreddit("EuSouOBabacaBOT").submit(
-                f"ERRO! {e.__cause__}", selftext="u/JakeWisconsin\n\n"+traceback.format_exc())
+            tools.logger(5, ex=traceback.format_exc())
 
 
 def backup():
@@ -267,30 +238,129 @@ def backup():
             folder = f"{settings['backup']}/{datetime.datetime.now().strftime('%Y-%m-%d/%H-%M-%S')}"
             src = "."
             shutil.copytree(src, folder, ignore=shutil.ignore_patterns("venv", ".", "__"))
-            print("Copiado")
-            reddit.subreddit("EuSouOBabacaBOT").submit(
-                f"Backup realizado",
-                selftext=f"Diretório: {folder}")
+            tools.logger(2, bprint=False, ex="Backup realizado")
         except:
-            reddit.subreddit("EuSouOBabacaBOT").submit(
-                f"Erro na função de backup",
-                selftext=f"{traceback.format_exc()}\n\nu/JakeWisconsin")
+            pass
         time.sleep(3600)
 
 
 def clearlog():
     while True:
-        time.sleep(86400)
+        time.sleep(config["clear_log"])
         open("log", "w+").write("")
-        reddit.subreddit("EuSouOBabacaBOT").submit(
-            f"Arquivo de log limpo",
-            selftext=f"Log limpo em {datetime.datetime.now().strftime('%d/%m/%Y às %H:%M:%S')}. u/JakeWisconsin")
+
+
+def textwall():
+    reddit.validate_on_submit = True
+    while True:
+        atime = datetime.datetime.now().timestamp()
+        try:
+            subcount = 0
+            submissons = reddit.subreddit(settings["subreddit"]).new(limit=int(settings["submissions"]))
+            for submission in submissons:
+                time.sleep(1)
+                subcount += 1
+                subid = submission.id
+
+                tools.logger(tp=3, num=subcount, sub_id=subid)
+
+                sublist = open('rid', 'r').readlines()
+                indx = -1
+                for i in sublist:
+                    indx += 1
+                    sublist[indx] = i.strip()
+
+                if subid not in sublist:
+                    try:
+                        body = submission.selftext
+                    except:
+                        body = ""
+                    paragraphs = 1
+                    sentences = 0
+
+                    # Detyerminar quantos parágrafos tem o texto
+                    index = -1
+                    paragraph_cond = False
+                    if body != "":
+                        for i in body:
+                            index += 1
+                            try:
+                                if i == "\n" and body[index+1] == "\n":
+                                    paragraphs += 1
+                                    sentences += 1
+                                    paragraph_cond = True
+                            except IndexError:
+                                pass
+
+                            # Quantas frases tem
+                            if i == "." and not paragraph_cond:
+                                sentences += 1
+
+                            paragraph_cond = False
+                    else:
+                        paragraphs = 0
+                        sentences = 0
+
+                    if sentences != 0 and paragraphs != 0:
+                        spp = sentences / paragraphs
+                    else:
+                        spp = 0
+
+                    if paragraphs < 2 or sentences <= 2:
+                        submission.mod.remove(mod_note="Parede de texto", spam=False)
+                        submission.reply(body=
+                                        f'Sua publicação '
+                                        f'foi removida por não se encaixar nos critérios '
+                                        f'de formatação para um texto legível. \n\n'
+                                        f'Certifique que o texto esteja dividido '
+                                        f'em parágrafos e possua a pontuação correta e poste novamente.\n\n'
+                                        f'Se houve um erro, contate meu criador: '
+                                        f'[JakeWisconsin]'
+                                        f'(https://www.reddit.com/u/JakeWisconsin)\n\n'
+                                        f'spp: {spp}\n\n'
+                                        f'paragrafos: {paragraphs}\n\n'
+                                        f'frases: {sentences}')
+                        tools.logger(tp=4, sub_id=subid, reason="Parede de texto")
+
+                        open("rid", "a").write(f"{subid}\n")
+            btime = datetime.datetime.now().timestamp()
+            tools.logger(tp=2, ex=f"textwall(): {btime-atime}s", bprint=False) 
+        except Exception:
+            tools.logger(tp=5, ex=traceback.format_exc())
 
 
 if __name__ == '__main__':
-    multiprocessing.Process(target=runtime, args=[]).start()
-    multiprocessing.Process(target=backup, args=[]).start()
-    multiprocessing.Process(target=clearlog, args=[]).start()
+    funcs = [runtime, backup, clearlog, textwall]
+    processes = [multiprocessing.Process(target=x, args=[]) for x in funcs]
+
+    '''processes = [multiprocessing.Process(target=runtime, args=[], name="Main"),
+    multiprocessing.Process(target=backup, args=[], name="Backup"),
+    multiprocessing.Process(target=clearlog, args=[], name="Clear-Log"),
+    multiprocessing.Process(target=textwall, args=[], name="Textwall")]'''
+    pids = [os.getpid()]
+
+    index = -1
+    for i in processes:
+        index += 1
+        i.start()
+        pids.append(i.pid)
+        print(f"Iniciado processo com o PID {i.pid} (função {funcs[index].__name__}())")
 
     while True:
-        continue
+        inp = input("=> ").upper().split(" ")
+        if len(inp) >= 1:
+            if inp[0] == "R":
+                config = json.load(open('config.json', 'r'))
+                api = json.load(open("api.json"))
+                splashes = json.load(open('splashes.json', 'r'))
+                print("Valores recarregados na memória.")
+            elif inp[0] == "E":
+                exit()
+            elif inp[0] == "RESTART":
+                for i in processes:
+                    i.terminate()
+
+                os.system(f"{config['python']} ./main.py")
+                break
+                
+                
--- tools.py
+++ tools.py
@@ -7,29 +7,24 @@ def logit(msg):
     open("log", "a").write(msg + "\n")
 
 
-def logger(tp, sub_id="", ex="", num="", reason=""):
-    current_time = datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
+def logger(tp, sub_id="", ex="", num="", reason="", bprint=False):
+    current_time = datetime.datetime.now().strftime("%d/%m/%Y às %H:%M:%S")
     msg = ""
 
     if tp == 0:
         msg = f"Comentário enviado em {sub_id}"
     elif tp == 1:
         msg = f"Comentário editado em {sub_id}"
-    elif tp == 2:
+    elif tp == 2 or tp == 5:
         msg = f"{ex}"
+        if tp == 5:
+            print(f"ERRO ({current_time}): {ex}")
+
+        if bprint:
+            print(ex)
     elif tp == 3:
         msg = f"Número {num} ({sub_id})"
     elif tp == 4:
         msg = f"{sub_id} foi removido. MOTIVO: {reason}"
 
     logit(f"[{current_time}] "+msg)
-    if os.name == "nt":
-        os.system("cls")
-    else:
-        os.system("clear")
-
-    print(f"""Beep bop!
-Ação mais recente: {msg}
-Horário do evento: {current_time}    
-Rodando em: {platform.system()} {''.join(platform.version())} {' '.join(platform.architecture())} 
-Python: {' '.join(platform.python_build())}""")
